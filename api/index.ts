d45f7a3f1e6378197b2e82632efd89ac20db17c2
Zahid Hasan <mrzahidxy.com>
2025-03-26 14:19:22 +0600

    add: health check

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { PORT } from "../src/secret";
 import rootRouter from "../src/routes";
 import cors from 'cors'
 import { errorMiddleware } from "../src/middleware/error";
+import { healthCheck } from "../src/controllers/healthCheck";
 
 const app: Express = express();
 app.use(express.json());
@@ -12,6 +13,8 @@ app.use(cors({ origin: '*' }))
 app.use("/api", rootRouter);
 
 app.use(errorMiddleware)
+
+app.get("/health", healthCheck);
 
 app.listen(PORT, () => {
   console.log(`The app listening on port ${PORT}`);
